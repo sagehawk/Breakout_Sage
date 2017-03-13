33ad26280da1e6daa9cae6c82c7ca8f4d1d761ae
Sage Hawk <SageHawk@sage.local>
2017-03-13 11:41:21 -0500

    Created ball object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -13,6 +13,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         createBackground()
+        makeBall()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -39,6 +40,34 @@ class GameScene: SKScene {
             let moveForever = SKAction.repeatForever(moveLoop)
             starsBackground.run(moveForever)
         }
+    }
+    
+    var ball = SKShapeNode()
+    
+    func makeBall() {
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x:frame.midX, y: frame.midY)
+        ball.strokeColor = UIColor.black
+        ball.fillColor = UIColor.yellow
+        ball.name = "ball"
+        
+        // physics shape matches ball image
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        //ignores all forces and impulses
+        ball.physicsBody?.isDynamic = false
+        //use precise collision detection
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        // no loss of energy from friction
+        ball.physicsBody?.friction = 0
+        //gravity is not a factor
+        ball.physicsBody?.affectedByGravity = false
+        //bounces fully off of other objects
+        ball.physicsBody?.restitution = 1
+        //does not slow down over time
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        
+        addChild(ball) // add ball object to the view
     }
     
 }
