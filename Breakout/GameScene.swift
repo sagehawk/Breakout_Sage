a015254aed71f35a2918fe1ff8a9c0575062056c
Sage Hawk <SageHawk@sage.local>
2017-03-14 11:33:44 -0500

    Created paddle dynamic behavior

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -28,11 +28,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+    }
     }
     
     func createBackground() {
