d5503c4d2883d0dafb18bdce4390ed00b9f91832
Sage Hawk <SageHawk@sage.local>
2017-04-06 11:12:38 -0500

    Added Jason's Multi brick Func

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -9,27 +9,23 @@
 import SpriteKit
 import GameplayKit
 
-let BallCategoryName = "ball"
-let PaddleCategoryName = "paddle"
-let BrickCategoryName = "brick"
-let BrickNodeCategoryName = "brickNode"
-
-let BallCategory   : UInt32 = 0x1 << 0 // 00000000000000000000000000000001
-let BottomCategory : UInt32 = 0x1 << 1 // 00000000000000000000000000000010
-let BrickCategory  : UInt32 = 0x1 << 2 // 00000000000000000000000000000100
-let PaddleCategory : UInt32 = 0x1 << 3 // 00000000000000000000000000001000
-
+//let BallCategoryName = "ball"
+//let PaddleCategoryName = "paddle"
+//let BrickCategoryName = "brick"
+//let BrickNodeCategoryName = "brickNode"
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var ball = SKShapeNode()
     var ballIcon = SKShapeNode()
     var paddle = SKSpriteNode()
-    var brick = SKSpriteNode()
+    var bricks = [SKSpriteNode]()
     
+    var lblGameOver = SKLabelNode()
+    var lblGameWon = SKLabelNode()
     var lblLives = SKLabelNode(fontNamed: "System")
     var lives:Int = 3
-    var numberOfBricks = 8
+    var numberOfBricks = 0
     
     struct game {
         static var IsOver : Bool = false
@@ -41,22 +37,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         createBackground()
         makeBall()
         makePaddle()
-        //makeBrick()
-        //createSprite()
-        //multiplyBrick()
-        addbricks()
+        multipleBricks()
         makeLoseZone()
         makeBallIcon()
-        makeLivesLabel()
+        makeLabels()
         ball.physicsBody?.isDynamic = true
         ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 3))
-        }
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
             paddle.position.x = location.x
+            if game.IsOver == true {
+                
+                resetGame()
         }
+    }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -66,21 +63,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
-    func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA.node?.name == BrickCategoryName ||
-            contact.bodyB.node?.name == "brick" {
+    /*func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node == brick ||
+            contact.bodyB.node == brick {
             brick.removeFromParent()
             numberOfBricks -= 1
             print("\(numberOfBricks)")
             if numberOfBricks == 0 {
-            print("You win!")
-            game.IsOver = true
-            ball.removeFromParent()
+                print("You win!")
+                game.IsOver = true
+                ball.removeFromParent()
             }
             
-            
-            
         }
+        
         if contact.bodyA.node?.name == "loseZone" ||
             contact.bodyB.node?.name == "lozeZone" {
             
@@ -90,26 +86,67 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             game.IsOver = false
             ball.physicsBody?.isDynamic = true
             ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 3))
-
+            
             if lives == 0 {
                 
-            print("You lose!")
-            ball.removeFromParent()
-            game.IsOver = true
+                print("You lose!")
+                ball.removeFromParent()
+                game.IsOver = true
+                
             }
+            resetGame()
             
-        if game.IsOver == true
-        {
-            //makeBall()
-            game.IsOver = false
-            ball.physicsBody?.isDynamic = true
-            ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 3))
+        }
+    }*/
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        for brick in bricks{
+            if contact.bodyA.node == brick ||
+                contact.bodyB.node == brick {
+                brick.removeFromParent()
+                numberOfBricks -= 1
+                
+                print("\(numberOfBricks)")
+                if numberOfBricks == 0 {
+                    print("You win!")
+                    game.IsOver = true
+                    ball.removeFromParent()
+                    gameWon()
+                }
 
+                
+                brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+                brick.physicsBody?.isDynamic = false
+                
+                
             }
         }
+            if contact.bodyA.node?.name == "loseZone" ||
+                contact.bodyB.node?.name == "loseZone" {
+                
+                self.updateScoreWithValue (value: 1)
+                ball.removeFromParent()
+                makeBall()
+                ball.physicsBody?.isDynamic = true
+                ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 3))
+                game.IsOver = false
+                ball.physicsBody?.isDynamic = true
+                ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 3))
+                
+                if lives == 0 {
+                    
+                    print("You lose!")
+                    ball.removeFromParent()
+                    game.IsOver = true
+                    
+                }
+                gameOver()
+            }
+        
+        
     }
     
-    /*func createBackground() {
+    func createBackground() {
         let stars = SKTexture(imageNamed: "stars")
         for i in 0...1 {
             let starsBackground = SKSpriteNode(texture: stars)
@@ -125,9 +162,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let moveForever = SKAction.repeatForever(moveLoop)
             starsBackground.run(moveForever)
         }
-    }*/
-    
-    func createBackground() {
+    }
+    /* func createBackground() {
         let skyline = SKTexture(imageNamed: "skyline")
         for i in 0...1 {
             let nintendoBackground = SKSpriteNode(texture: skyline)
@@ -143,17 +179,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let moveForever = SKAction.repeatForever(moveLoop)
             nintendoBackground.run(moveForever)
         }
-    }
+    } */
     
     func makeBall() {
         ball = SKShapeNode(circleOfRadius: 10)
         ball.position = CGPoint(x:frame.midX, y: -150)
         ball.strokeColor = UIColor.black
-        ball.fillColor = UIColor.yellow
+        ball.fillColor = UIColor.white
         ball.name = "ball"
         
         // physics shape matches ball image
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 9)
         //ignores all forces and impulses
         ball.physicsBody?.isDynamic = false
         //use precise collision detection
@@ -179,21 +215,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(paddle)
     }
     
-    func makeBrick() {
-        brick = SKSpriteNode(color: UIColor.blue, size: CGSize(width: frame.width/5, height: frame.height/25))
-        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 30)
-        brick.name = "brick"
-        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
-        brick.physicsBody?.isDynamic = false
-        addChild(brick)
-        numberOfBricks += 1
-        
-    }
-    
-    
-    
     func makeLoseZone() {
-        let loseZone = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width, height: 50))
+        let loseZone = SKSpriteNode(color: UIColor.white, size: CGSize(width: frame.width, height: 50))
         loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
         loseZone.name = "loseZone"
         loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
@@ -201,70 +224,102 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(loseZone)
     }
     
-    func skActions() {
-        makeBrick()
-        let moveRight = SKAction.moveBy(x: 100,
-                                     y: 0,
-                                     duration: 0.0)
-        let moveLeft = SKAction.moveBy(x: -100,
-                                        y: 0,
-                                        duration: 0.0)
-        let moveDown = SKAction.moveBy(x: 0, y: -100, duration: 0.0)
-        brick.run(moveRight)
-        makeBrick()
-        brick.run(moveLeft)
-        makeBrick()
-        brick.run(moveDown)
-        makeBrick()
-        brick.run(moveRight)
-        brick.run(moveDown)
-    }
     func makeBallIcon() {
         ballIcon = SKShapeNode(circleOfRadius: 10)
         ballIcon.position = CGPoint(x: -185, y: 355)
         ballIcon.strokeColor = UIColor.black
-        ballIcon.fillColor = UIColor.yellow
+        ballIcon.fillColor = UIColor.white
         ballIcon.name = "BallIcon"
         addChild(ballIcon)
     }
     
-    func makeLivesLabel() {
-         //The variable holding the score.
-        
+    func makeLabels() {
+        //The variable holding the score.
         lblLives = SKLabelNode()
-        lblLives.text = "\(lives)"
+        lblLives.text = "X \(lives)"
         lblLives.fontSize = 20
         lblLives.position = CGPoint(x: -153, y: 347)
-        
         addChild(lblLives)
+        
+        lblGameOver = SKLabelNode()
+        lblGameOver.text = "GAME OVER"
+        lblGameOver.fontSize = 50
+        lblGameOver.position = CGPoint(x: 0, y: 0)
+
+        lblGameWon = SKLabelNode()
+        lblGameWon.text = "GAME WON"
+        lblGameWon.fontSize = 50
+        lblGameWon.position = CGPoint(x: 0, y: 0)
+
+        
     }
+    
     func updateScoreWithValue (value: Int) {
         lives -= value
-        lblLives.text = "\(lives)"
+        lblLives.text = "X \(lives)"
     }
-    /*func addbricks() {
-        // 1
-        let numberOfbricks = 8
-        let brickWidth = SKSpriteNode(imageNamed: "brick").size.width
-        let totalbricksWidth = brickWidth * CGFloat(numberOfbricks)
-        // 2
-        let xOffset = (frame.width - totalbricksWidth) / 2
-        // 3
-        for i in 0..<numberOfbricks {
-            brick = SKSpriteNode(imageNamed: "brick.png")
-            brick.position = CGPoint(x: -210 /*xOffset*/ + CGFloat(CGFloat(i) + 0.5) * brickWidth,
-                                     y: 300)//frame.height * 0.8)
-            
-            brick.physicsBody = SKPhysicsBody(rectangleOf: brick.frame.size)
-            brick.physicsBody!.allowsRotation = false
-            brick.physicsBody!.friction = 0.0
-            brick.physicsBody!.affectedByGravity = false
-            brick.physicsBody!.isDynamic = false
-            brick.name = BrickCategoryName
-            brick.physicsBody!.categoryBitMask = BrickCategory
-            brick.zPosition = 2
-            addChild(brick)
-        }
 
-    }*/
+    func gameOver() {
+        
+        if game.IsOver == true
+        {
+            self.removeAllActions()
+            self.removeAllChildren()
+            addChild(lblGameOver)
+            //add this to on tap gesture recognizer
+            //game.IsOver = false
+            //ball.physicsBody?.isDynamic = true
+            //ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 3))
+            
+        }
+    }
+    
+    func gameWon() {
+        if game.IsOver == true
+        {
+            self.removeAllActions()
+            self.removeAllChildren()
+            addChild(lblGameWon)
+        }
+    }
+    
+    func multipleBricks() {
+        let countB = Int(frame.width) / 100
+        let height = Int(frame.maxY)
+        let xOffset = (Int(frame.width) - (countB * 100)) / 10 + Int(frame.minX) + 50
+        for x in 0..<countB{makeBrick(x: x * 100 + xOffset, y: height - 100) }
+        for x in 0..<countB{makeBrick(x: x * 100 + xOffset, y: height - 250) }
+        for x in 0..<countB{makeBrick(x: x * 100 + xOffset, y: height - 400) }
+        
+    }
+    
+    func makeBrick(x: Int, y: Int) {
+        let brick = SKSpriteNode(imageNamed: "brick")
+        
+        brick.position = CGPoint(x: x, y: y)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
+        numberOfBricks += 1
+        bricks.append(brick)
+    }
+    func resetGame()
+    {
+        game.IsOver = false
+        lives = 3
+        numberOfBricks = 0
+        self.removeAllActions()
+        self.removeAllChildren()
+        createBackground()
+        makeBall()
+        makePaddle()
+        multipleBricks()
+        makeLoseZone()
+        makeBallIcon()
+        makeLabels()
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 3))
+
+    }
 }
